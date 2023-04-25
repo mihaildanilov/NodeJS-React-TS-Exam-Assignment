d20a0c8efebd30ee89fb38cce499ec8902b12a4c
Mihail Danilov <45045888+mihaildanilov@users.noreply.github.com>
2023-04-25 23:32:53 +0300

    Product fetching implemented. (#4)
    Product fetching from backend local storage implemented.
    Added loading and message box.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router/Router';
 import { ContextProvider } from './context/ContextProvider';
+import axios from 'axios';
 
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<ContextProvider>
