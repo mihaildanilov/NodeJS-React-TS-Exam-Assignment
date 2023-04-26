b60b514950603bd4ad10f416d4194a3be331b577
Mihail Danilov <mihaildanilov793@gmail.com>
2023-04-26 22:57:16 +0300

    Product page created.
    A product page was created and added product status toast. Added description sample for the product.

--- backend/src/data/ProductListData.ts
+++ backend/src/data/ProductListData.ts
@@ -1,7 +1,6 @@
 export interface ProductListItemProps {
 	name: string;
 	slug: string;
-
 	category: string;
 	brand: string;
 	imageSrc: string;
@@ -19,21 +18,22 @@ export const ProductListData: ProductListItemProps[] = [
 		name: 'Nike Air VaporMax 2021 “Air Pressure”',
 		slug: 'nike-vapormax-air-pressure',
 		category: 'Sneakers',
-		imageSrc: './images/air-pressure.jpeg',
+		imageSrc: '/images/air-pressure.jpeg',
 		imageAlt: 'Side view of Nike Air VaporMax 2021 “Air Pressure”',
 		price: 210,
-		countInStock: 10,
+		countInStock: 0,
 		brand: 'Nike',
 		rating: 3.5,
 		numberOfReviews: 10,
-		description: 'High quality sneakers',
+		description:
+			'The Nike Air VaporMax 2021 "Air Pressure" is a sleek and stylish sneaker that\'s designed for comfort and performance. This shoe features a futuristic look that\'s sure to turn heads, with its Yellow and white color scheme and unique, textured design.',
 		color: 'White/Old Royal-Yellow Ochre',
 	},
 	{
 		name: 'Nike Air VaporMax 2023 Flyknit “Baltic Blue”',
 		slug: 'nike-vapormax-baltic-blue',
 		category: 'Sneakers',
-		imageSrc: './images/baltic-blue.jpeg',
+		imageSrc: '/images/baltic-blue.jpeg',
 		imageAlt: 'Side view of Nike Air VaporMax 2023 Flyknit “Baltic Blue”',
 		price: 210,
 		countInStock: 10,
@@ -47,7 +47,7 @@ export const ProductListData: ProductListItemProps[] = [
 		name: 'Nike Air VaporMax 2023 Flyknit',
 		slug: 'nike-vapormax-flyknit-black',
 		category: 'Sneakers',
-		imageSrc: './images/flyknit-black.jpeg',
+		imageSrc: '/images/flyknit-black.jpeg',
 		imageAlt: 'Side view of Nike Air VaporMax 2023 Flyknit',
 		price: 210,
 		countInStock: 10,
@@ -61,7 +61,7 @@ export const ProductListData: ProductListItemProps[] = [
 		name: 'Nike Air VaporMax 2023 Flyknit “Pure Platinum”',
 		slug: 'nike-vapormax-pure-platinum',
 		category: 'Sneakers',
-		imageSrc: './images/pure-platinum.jpeg',
+		imageSrc: '/images/pure-platinum.jpeg',
 		imageAlt: 'Side view of Nike Air VaporMax 2023 Flyknit “Pure Platinum”',
 		price: 210,
 		countInStock: 10,
@@ -75,7 +75,7 @@ export const ProductListData: ProductListItemProps[] = [
 		name: 'Nike Air VaporMax 2021 “Air Pressure”',
 		slug: 'nike-vapormax-air-pressure',
 		category: 'Sneakers',
-		imageSrc: './images/air-pressure.jpeg',
+		imageSrc: '/images/air-pressure.jpeg',
 		imageAlt: 'Side view of Nike Air VaporMax 2021 “Air Pressure”',
 		price: 210,
 		countInStock: 10,
@@ -89,7 +89,7 @@ export const ProductListData: ProductListItemProps[] = [
 		name: 'Nike Air VaporMax 2023 Flyknit “Baltic Blue”',
 		slug: 'nike-vapormax-baltic-blue',
 		category: 'Sneakers',
-		imageSrc: './images/baltic-blue.jpeg',
+		imageSrc: '/images/baltic-blue.jpeg',
 		imageAlt: 'Side view of Nike Air VaporMax 2023 Flyknit “Baltic Blue”',
 		price: 210,
 		countInStock: 10,
@@ -103,7 +103,7 @@ export const ProductListData: ProductListItemProps[] = [
 		name: 'Nike Air VaporMax 2023 Flyknit',
 		slug: 'nike-vapormax-flyknit-black',
 		category: 'Sneakers',
-		imageSrc: './images/flyknit-black.jpeg',
+		imageSrc: '/images/flyknit-black.jpeg',
 		imageAlt: 'Side view of Nike Air VaporMax 2023 Flyknit',
 		price: 210,
 		countInStock: 10,
@@ -117,7 +117,7 @@ export const ProductListData: ProductListItemProps[] = [
 		name: 'Nike Air VaporMax 2023 Flyknit “Pure Platinum”',
 		slug: 'nike-vapormax-pure-platinum',
 		category: 'Sneakers',
-		imageSrc: './images/pure-platinum.jpeg',
+		imageSrc: '/images/pure-platinum.jpeg',
 		imageAlt: 'Side view of Nike Air VaporMax 2023 Flyknit “Pure Platinum”',
 		price: 210,
 		countInStock: 10,
--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,9 +1,99 @@
+import { useParams } from 'react-router-dom';
 import PageComponent from '../components/PageComponent';
+import MessageBox from '../components/MessageBox';
+import { ApiError } from '../types/ApiError';
+import { getError } from '../utils/utils';
+import LoadingBox from '../components/LoadingBox';
+import { useGetProductDetailsBySlugQuery } from '../hooks/productHook';
+import PageNotFound from './PageNotFound';
+
+import Rating from '../components/Rating';
+import StatusToast from './StatusToast';
 
 const ProductPage = () => {
-	return (
+	const params = useParams();
+	const { slug } = params;
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+	const { data: product, isLoading, error } = useGetProductDetailsBySlugQuery(slug!);
+
+	return isLoading ? (
+		<LoadingBox text="Action in progress" />
+	) : error ? (
+		<MessageBox message={getError(error as ApiError)} />
+	) : !product ? (
+		<PageNotFound />
+	) : (
 		<PageComponent title="Product page">
-			<div>Product Description</div>
+			<div className="bg-white">
+				<div className="pt-6">
+					{/* Product Image */}
+					<div className="max-w-2xl  lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+						<img
+							src={product.imageSrc}
+							alt={product.imageAlt}
+							className="h-full w-full object-cover object-center"
+						/>
+						<div className="mt-4 lg:row-span-3 lg:mt-0 mx-[100px]">
+							<h2 className="sr-only">Product information</h2>
+							<p className="text-3xl  text-gray-900">Price: {product.price}$</p>
+							{/* Reviews */}
+							<div className="mt-6">
+								<h3 className="sr-only">Reviews</h3>
+								<div className="flex items-center">
+									<div className="flex items-center">
+										<Rating rating={product.rating} />
+									</div>
+
+									<p className="ml-3 text-sm font-medium text-indigo-600">
+										{product.numberOfReviews} reviews
+									</p>
+								</div>
+							</div>
+							<form className="pt-">
+								{product.countInStock > 0 ? (
+									<>
+										<StatusToast message="In stock" />
+										<button
+											type="button"
+											className="py-3 mt-4 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600   transition-all text-sm ">
+											Add to cart
+											<svg
+												className="w-3.5 h-3.5"
+												xmlns="http://www.w3.org/2000/svg"
+												width="16"
+												height="16"
+												fill="currentColor"
+												viewBox="0 0 16 16">
+												<path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717L5.07 1.243zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3z" />
+											</svg>
+										</button>
+									</>
+								) : (
+									<StatusToast message="Out stock" />
+								)}
+							</form>
+						</div>
+					</div>
+					{/* Product info */}
+					<div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+						<div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+							<h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+								{product.name}
+							</h1>
+						</div>
+						<div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+							{/* Description and details */}
+							<div>
+								<h3 className="sr-only">Description</h3>
+
+								<div className="space-y-6">
+									<p className="text-base text-gray-900">{product.description}</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</PageComponent>
 	);
 };
