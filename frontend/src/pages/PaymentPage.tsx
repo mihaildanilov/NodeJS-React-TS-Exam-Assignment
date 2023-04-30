1299ec75bfa19dfb07a6750262c346affb194652
Mihail Danilov <mihaildanilov793@gmail.com>
2023-05-01 00:31:12 +0300

    Added payment route to the router

---
+++ frontend/src/pages/PaymentPage.tsx
@@ -0,0 +1,13 @@
+import PageComponent from '../components/PageComponent';
+
+const PaymentPage = () => {
+	return (
+		<PageComponent title="Payment">
+			<div className="isolate bg-white px-6 sm:py-8 lg:px-8 h-screen lg:h-[calc(100vh-12.5rem)]   ">
+				<div>Payment page</div>
+			</div>
+		</PageComponent>
+	);
+};
+
+export default PaymentPage;
