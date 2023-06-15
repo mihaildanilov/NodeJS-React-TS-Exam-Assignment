a1621255752b5e4cf8543bd103f69fe80cdb2542
Mihail Danilov <mihaildanilov793@gmail.com>
2023-06-16 00:59:26 +0300

    About Us page added

--- frontend/src/pages/customer/AboutUsPage.tsx
+++ frontend/src/pages/customer/AboutUsPage.tsx
@@ -1,10 +1,40 @@
-import { PageComponent } from '../layouts';
-
 const AboutUs = () => {
 	return (
-		<PageComponent title="About us">
-			<div>About Us Page content</div>
-		</PageComponent>
+		<div className="bg-white">
+			<div className="relative isolate px-6 lg:px-8">
+				<div className="mx-auto max-w-2xl py-24 sm:py-32 lg:py-48">
+					<div className="text-center">
+						<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+							Welcome to Kicks Avenue, where style and comfort meet.
+						</h1>
+						<p className="mt-6 text-lg leading-8 text-gray-600">
+							Shop our collection of sneakers and clothing for men and women,
+							featuring the latest trends and brands. Find your perfect fit and
+							elevate your wardrobe today.
+						</p>
+						<p className="mt-6 text-lg leading-8 text-gray-600">
+							Kicks Avenue is your premier destination for the latest sneakers and
+							designer clothes. We curate a collection of high-quality products that
+							cater to diverse styles and tastes. Our team of fashion enthusiasts
+							ensures that only the best items make it to our virtual shelves.
+						</p>
+						<p className="mt-6 text-lg leading-8 text-gray-600">
+							At Kicks Avenue, we believe that fashion should be accessible to
+							everyone. That&apos;s why we offer competitive prices without
+							compromising on quality. With our user-friendly website, secure payment
+							options, and dedicated customer support, your shopping experience is
+							effortless and enjoyable.
+						</p>
+						<p className="mt-6 text-lg leading-8 text-gray-600">
+							Join our community of trendsetters and sneakerheads. Follow us on social
+							media for updates on new releases and exclusive promotions. Thank you
+							for choosing Kicks Avenue as your fashion destination. Walk with style
+							and confidence!
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
 
