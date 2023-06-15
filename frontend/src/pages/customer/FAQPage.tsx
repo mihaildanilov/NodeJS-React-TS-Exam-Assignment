c6e4c3b523e6ba2c915037c203623085f2f1184a
Mihail Danilov <45045888+mihaildanilov@users.noreply.github.com>
2023-06-16 01:30:06 +0300

    Update FAQPage.tsx (#85)

--- frontend/src/pages/customer/FAQPage.tsx
+++ frontend/src/pages/customer/FAQPage.tsx
@@ -1,10 +1,202 @@
-import { PageComponent } from '../layouts';
-
 const FAQPage = () => {
 	return (
-		<PageComponent title="FAQ">
-			<div>FAQ Page content</div>
-		</PageComponent>
+		<section className="bg-white pt-[5.5rem] ">
+			<div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-10 lg:px-6">
+				<h2 className="mb-8 text-4xl font-extrabold tracking-tight text-gray-900 ">
+					Frequently asked questions
+				</h2>
+				<div className="grid border-t border-gray-200 pt-8 text-left  md:grid-cols-2 md:gap-16">
+					<div>
+						<div className="mb-10">
+							<h3 className="mb-4 flex items-center text-lg font-medium text-gray-900 ">
+								<svg
+									className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 "
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg">
+									<path
+										fillRule="evenodd"
+										d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+										clipRule="evenodd"
+									/>
+								</svg>
+								What products do you offer in your ecommerce shop?
+							</h3>
+							<p className="text-gray-500 ">
+								We offer a wide range of products, including sneakers, athletic
+								shoes, casual footwear, modern clothing, streetwear, accessories,
+								and other fashion-forward items.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h3 className="mb-4 flex items-center text-lg font-medium text-gray-900 ">
+								<svg
+									className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 "
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg">
+									<path
+										fillRule="evenodd"
+										d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+										clipRule="evenodd"
+									/>
+								</svg>
+								Are the products in your shop authentic?
+							</h3>
+							<p className="text-gray-500 ">
+								Yes, all the products we sell in our ecommerce shop are 100%
+								authentic. We source our inventory directly from authorized
+								retailers and trusted suppliers.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h3 className="mb-4 flex items-center text-lg font-medium text-gray-900 ">
+								<svg
+									className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 "
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg">
+									<path
+										fillRule="evenodd"
+										d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+										clipRule="evenodd"
+									/>
+								</svg>
+								How do I place an order?
+							</h3>
+							<p className="text-gray-500 ">
+								To place an order, simply browse our website and select the items
+								you wish to purchase. Add them to your cart and proceed to the
+								checkout page. Follow the instructions to provide your shipping
+								address, payment details, and any other required information.
+							</p>
+							<p className="text-gray-500 ">
+								Feel free to{' '}
+								<a
+									href="/contact"
+									className="font-medium underline hover:no-underline"
+									target="_blank"
+									rel="noreferrer">
+									contact us
+								</a>{' '}
+								and we&apos;ll help you out as soon as we can.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h3 className="mb-4 flex items-center text-lg font-medium text-gray-900 ">
+								<svg
+									className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 "
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg">
+									<path
+										fillRule="evenodd"
+										d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+										clipRule="evenodd"
+									/>
+								</svg>
+								Do you offer international shipping?
+							</h3>
+							<p className="text-gray-500 ">
+								Yes, we offer international shipping to many countries. However,
+								availability may vary depending on your location. You can check if
+								we ship to your country during the checkout process by entering your
+								address details.
+							</p>
+						</div>
+					</div>
+					<div>
+						<div className="mb-10">
+							<h3 className="mb-4 flex items-center text-lg font-medium text-gray-900 ">
+								<svg
+									className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 "
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg">
+									<path
+										fillRule="evenodd"
+										d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+										clipRule="evenodd"
+									/>
+								</svg>
+								How much does shipping cost?
+							</h3>
+							<p className="text-gray-500">
+								Shipping costs are calculated based on your location and the weight
+								of your order. The exact shipping cost will be displayed at checkout
+								before you finalize your purchase.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h3 className="mb-4 flex items-center text-lg font-medium text-gray-900 ">
+								<svg
+									className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 "
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg">
+									<path
+										fillRule="evenodd"
+										d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+										clipRule="evenodd"
+									/>
+								</svg>
+								What is your return policy?
+							</h3>
+							<p className="text-gray-500 ">
+								We have a hassle-free return policy. If you are not satisfied with
+								your purchase, you can return the item within a specified period
+								(usually 30 days) from the delivery date. The product should be in
+								its original condition with all tags and packaging intact. Please
+								review our detailed return policy on our website for more
+								information.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h3 className="mb-4 flex items-center text-lg font-medium text-gray-900 ">
+								<svg
+									className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 "
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg">
+									<path
+										fillRule="evenodd"
+										d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+										clipRule="evenodd"
+									/>
+								</svg>
+								What if the item I want is out of stock?
+							</h3>
+							<p className="text-gray-500 ">
+								If an item you desire is out of stock, you can join the waitlist or
+								sign up for our newsletter. This way, you&apos;ll receive updates
+								when the item becomes available again.
+							</p>
+						</div>
+						<div className="mb-10">
+							<h3 className="mb-4 flex items-center text-lg font-medium text-gray-900 ">
+								<svg
+									className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 "
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg">
+									<path
+										fillRule="evenodd"
+										d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+										clipRule="evenodd"
+									/>
+								</svg>
+								How can I contact your customer support?
+							</h3>
+							<p className="text-gray-500 ">
+								If you have any questions, concerns, or need assistance, you can
+								reach our customer support team through the contact form provided on
+								our website.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 };
 
