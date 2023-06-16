8bf531df35b9fe0c7e13616a7d8c417429f2f279
Mihail Danilov <mihaildanilov793@gmail.com>
2023-06-16 03:33:33 +0300

    Delete product functionality added
    Implemented delete product by slug.
    Modified warning modal to take custom styles for button and not trigger success toast if the message is not provided.
    Deleted useless dependencies in the backend.

--- frontend/src/hooks/productHook.ts
+++ frontend/src/hooks/productHook.ts
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 import apiClient from '../api/apiClient';
 import { ProductListItemProps } from '../types';
 import { useQuery } from '@tanstack/react-query';
@@ -14,3 +15,18 @@ export const useGetProductDetailsBySlugQuery = (slug: string) =>
 		queryFn: async () =>
 			(await apiClient.get<ProductListItemProps>(`api/products/slug/${slug}`)).data,
 	});
+
+export const deleteProduct = async (slug: string) => {
+	try {
+		const response = await apiClient.delete(`/api/products/slug/${slug}`);
+		if (response.status === 200) {
+			toast.success('Product deleted successfully');
+		} else if (response.status === 404) {
+			toast.error('Product not found');
+		} else {
+			toast.error('Internal server error');
+		}
+	} catch (error) {
+		toast.error('Error occurred');
+	}
+};
