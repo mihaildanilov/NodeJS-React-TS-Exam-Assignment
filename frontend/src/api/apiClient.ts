3960dbdfa4cdeb63d1459b1f61a50507297c625d
Mihail Danilov <mihaildanilov793@gmail.com>
2024-02-19 23:10:10 +0200

    92 fix typos (#107)
    * Fixed typos
    
    * fix(backend): fix type incompletion
    
    * feat: add origin and api routes
    
    * Update FrontendWorkflow.js.yml

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -2,10 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-	baseURL:
-		process.env.NODE_ENV === 'development'
-			? 'http://localhost:4000/'
-			: 'kicks-avenue-api.mihaildanilov.com',
+	baseURL: process.env.REACT_APP_API_URL,
 	headers: {
 		'Content-type': 'application/json',
 	},
